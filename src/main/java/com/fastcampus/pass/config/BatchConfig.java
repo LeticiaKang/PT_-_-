a35b9d37f754f536499b6946f8aac4976fe7a0f6
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2024-01-27 12:22:21 +0900

    #14 - 간단하게 구성한 배치 주석처리
    - 메타데이터 생성을 위해서 @EnableBatchProcessing가 없어야 하지만, 배치를 돌릴 때, 필요하여 추가함
    - 처음에 간단하게 만들었던 passJob, Step은 주석 처리하여 충돌을 방지함

--- src/main/java/com/fastcampus/pass/config/BatchConfig.java
+++ src/main/java/com/fastcampus/pass/config/BatchConfig.java
@@ -1,9 +1,20 @@
 package com.fastcampus.pass.config;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.PlatformTransactionManager;
 
 /*
  * @EnableBatchProcessing
@@ -12,9 +23,29 @@ import org.springframework.context.annotation.Configuration;
  */
 
 @Configuration  // 환경 설정 관련 Class 임을 명시
+@EnableBatchProcessing  //초기 메타 데이터 생성시 주석 처리하고 돌려야 메타데이터가 생성됨
 @EnableConfigurationProperties(BatchProperties.class)
 public class BatchConfig {
+//    @Bean
+//    public Job passJob(JobRepository jobRepository, PlatformTransactionManager transactionManager,
+//                       Step passStep) {
+//        return new JobBuilder("passJob", jobRepository)
+//                .start(passStep)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step passStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+//        return new StepBuilder("passStep", jobRepository)
+//                .tasklet((StepContribution contribution, ChunkContext chunkContext) -> {
+//                    System.out.println("테스트 성공!");
+//                    return RepeatStatus.FINISHED;
+//                }, transactionManager)
+//                .build();
+//    }
+
 }
+
 
 
 
