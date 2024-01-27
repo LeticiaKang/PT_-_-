098580cc823e3a612ca646978589cf22c846cae9
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2024-01-27 12:23:39 +0900

    #14 - 간단하게 구성한 배치 주석처리
    - 메타데이터 생성을 위해서 @EnableBatchProcessing가 없어야 하지만, 배치를 돌릴 때, 필요하여 추가함
    - 처음에 간단하게 만들었던 passJob, Step은 주석 처리하여 충돌을 방지함

--- src/main/java/com/fastcampus/pass/PassBatchApplication.java
+++ src/main/java/com/fastcampus/pass/PassBatchApplication.java
@@ -1,19 +1,13 @@
 package com.fastcampus.pass;
 
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.transaction.PlatformTransactionManager;
 
-@EnableBatchProcessing  // 배치를 돌리기 위함
 @SpringBootApplication
 public class PassBatchApplication {
 
@@ -21,35 +15,10 @@ public class PassBatchApplication {
 		SpringApplication.run(PassBatchApplication.class, args);
 	}
 
-	// 배치를 만들기 위해선 스템을 만들고, 스텝을 만들기 위해선, 잡을 만들어야 한다
-
-	// 잡 구현
-	@Bean
-	public Job passJob(JobRepository jobRepository, Step passStep) {
-		return new JobBuilder("passStep", jobRepository)
-				.start(passStep)  // start(스텝)
-				.build();
-	}
-
-	// 스템 구현
-	@Bean
-	public Step passStep(final JobRepository jobRepository,
-						 final Tasklet passTasklet,
-						 final PlatformTransactionManager platformTransactionManager) {
-		return new StepBuilder("passStep", jobRepository)
-				.tasklet(passTasklet, platformTransactionManager)
-				.build();
-	}
-
-	//Tasklet 구현
-	@Bean
-	public Tasklet passTasklet() {
-		return ((contribution, chunkContext) -> {
-			System.out.println("Execute PassStep");
-			return RepeatStatus.FINISHED;
-		});
-	}
-
+//	@Bean
+//	ApplicationRunner applicationRunner(JobLauncher jobLauncher, Job passJob) {
+//		return args -> jobLauncher.run(passJob, new JobParameters());
+//	}
 
 }
 
